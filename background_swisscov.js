d9713c5e5d7bd50f558c4b7c22ba8b233db9b8a1
Raphaël Grolimund <grolimur@nous4.ch>
2021-04-09 09:14:43 +0200

    Updated script to search in swisscovery | HES-SO

--- background_swisscov.js
+++ background_swisscov.js
@@ -1,10 +1,10 @@
 chrome.contextMenus.create({
-    "title": "Die ausgewählte Textstelle %s in Swisscovery suchen",
+    "title": "Rechercher %s dans Swisscovery | HES-SO",
     "type": "normal",
     "contexts": ["selection"],
     "onclick": function(info) {
         chrome.tabs.create({
-            url: 'https://uzb.swisscovery.slsp.ch/discovery/search?query=any,contains,'+encodeURIComponent(info.selectionText)+'&tab=41SLSP_UZB_DN_and_CI&search_scope=DN_and_CI&vid=41SLSP_UZB:UZB&offset=0'
+            url: 'https://hesso.swisscovery.slsp.ch/discovery/search?query=any,contains,'+encodeURIComponent(info.selectionText)+'&tab=&tab=41SLSP_HES_DN_and_CI&search_scope=DN_and_CI&vid=41SLSP_HES:VU1&offset=0'
         });
     }
 });
